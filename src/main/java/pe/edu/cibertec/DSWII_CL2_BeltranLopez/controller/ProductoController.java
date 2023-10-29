c92d10484f60aaf3f070ec8a8386229c63fc4daa
GianBeltran <Gbeltranl0506@gmail.com>
2023-10-28 21:48:58 -0500

    Tercer commit creo

---
+++ src/main/java/pe/edu/cibertec/DSWII_CL2_BeltranLopez/controller/ProductoController.java
@@ -0,0 +1,56 @@
+package pe.edu.cibertec.DSWII_CL2_BeltranLopez.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import pe.edu.cibertec.DSWII_CL2_BeltranLopez.exception.ResourceNotFoundException;
+import pe.edu.cibertec.DSWII_CL2_BeltranLopez.model.Producto;
+import pe.edu.cibertec.DSWII_CL2_BeltranLopez.service.ProductoService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/producto")
+@AllArgsConstructor
+public class ProductoController {
+
+    private ProductoService productoService;
+
+    @GetMapping("")
+    public ResponseEntity<List<Producto>> listarProductos(){
+        List<Producto> productoList = new ArrayList<>();
+        productoService.listarProductos().forEach(productoList::add);
+        if (productoList.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(productoList, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Producto> obtenerProducto(@PathVariable("id") Integer id){
+        Producto producto = productoService.obtenerProductoPorId(id)
+                .orElseThrow(() -> new ResourceNotFoundException("El producto con el Id Nro. "+
+                        id + " no existe."));
+        return new ResponseEntity<>(producto, HttpStatus.OK);
+    }
+
+    @PostMapping("")
+    public ResponseEntity<Producto> registrarProducto(@RequestBody Producto producto){
+        return new ResponseEntity<>(productoService.guardar(producto), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Producto> actualizarProducto(@PathVariable("id") Integer id, @RequestBody Producto producto){
+        Producto oldProducto = productoService.obtenerProductoPorId(id)
+                .orElseThrow(() -> new ResourceNotFoundException("El producto con el Id Nro. "+
+                        id + " no existe."));
+        oldProducto.setNombproducto(producto.getNombproducto());
+        oldProducto.setDescproducto(producto.getDescproducto());
+        oldProducto.setCantidad(producto.getCantidad());
+        oldProducto.setFechaVenc(producto.getFechaVenc());
+
+        return new ResponseEntity<>(productoService.guardar(oldProducto), HttpStatus.OK);
+    }
+}
